89d8b5c28cccc3a890e351dcadcf68ced2a8a04a
ZuriGa <zurisadailopezmartinez@gmail.com>
2023-10-27 15:47:20 -0700

    3rd test passes, it will add word boop if number 2 gets enter

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 // // // //Business Logic
-function beepBoop(number){
+function beepBoop(number) {
     let result = " ";
     for (let i = 0; i <= number; i++) {
-        let transformedNum =i.toString();
+        let transformedNum = i.toString();
         if (transformedNum.includes("1")) {
             transformedNum = "Beep!";
+        } else if (transformedNum.includes("2")) {
+            transformedNum = "Boop!";
         }
+
         result += transformedNum + " ";
     }
     return result.trim();
